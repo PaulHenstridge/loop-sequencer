a5863ae59bbd165c425e3c597630add7523292e0
Paul Henstridge <PaulHenstridge"gmail.com>
2022-06-26 11:58:11 +0100

    basics working

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const container = document.querySelector('.container')
-const notes = ['G6', 'F6', 'E6', 'D6', 'C6', 'B5', 'A5', 'G4', 'F4', 'E4', 'D4', 'C4', 'B3', 'A3', 'G3', 'F3', 'E3', 'D3', 'C3', 'B2', 'A2']
+const notes = [
+    'G6', 'F6', 'E6', 'D6', 'C6', 'B5', 'A5',
+    'G4', 'F4', 'E4', 'D4', 'C4', 'B3', 'A3',
+    'G3', 'F3', 'E3', 'D3', 'C3', 'B2', 'A2',
+    'G5', 'F4', 'E4', 'D2', 'C2', 'B1', 'A1'
+]
+// find more elegant way of creating above array
 
+// the html to form each check box label into a nice clickable thing that indicates when it is selected
+// and can show time in the loop (e.g a background on each active one, making a sort ogf column?)
 
-function setup(notesArray, noteRows = 7, beats = 16) {
+const checkHtml = `
+<div class="cxBox"></div>
+`
+
+function setup(notesArray, noteRows = 28, beats = 16) {
     for (let i = 0; i < noteRows; i++) {
         let soundRow = document.createElement('div')
         soundRow.classList.add('row')
@@ -11,21 +23,24 @@ function setup(notesArray, noteRows = 7, beats = 16) {
         infoBox.classList.add('info-box')
         infoBox.innerText = notesArray[i]
 
-
+        // i want to expand the label element to style the checkboxes
         for (let j = 0; j < beats; j++) {
-            let check = document.createElement('input')
-            check.setAttribute('type', 'checkbox')
-            if (j % 4 === 3) {
+            let check = document.createElement('div')
+            check.setAttribute('data-status', 'off')
+            check.classList.add('check')
+            check.setAttribute('id', `r${i}b${j}`) //r=row, b=beat, so eg: r2b5
+            check.addEventListener('click', () => {
+                check.dataset.status = check.dataset.status === 'off' ? 'on' : 'off'
+                check.style.backgroundColor = check.dataset.status === 'off' ? 'black' : 'yellow'
+            })
+            //seperate 4 beat bars
+            if (j % 4 === 3) {                 // if (!(j+1 %4))
                 check.style.marginRight = '3vw'
             }
             soundRow.appendChild(check)
-
-            // if (j % 4 === 3 && j !== beats - 1) {
-            //     let barSeparator = document.createElement('div')
-            //     barSeparator.classList.add('bar-separator')
-            //     soundRow.appendChild(barSeparator)
-            // }
+            // soundRow.appendChild(checkLabel)
         }
+
         container.appendChild(soundRow)
         container.insertBefore(infoBox, soundRow)
         if (i % 7 === 6) {
@@ -36,7 +51,12 @@ function setup(notesArray, noteRows = 7, beats = 16) {
     }
 }
 
-setup(notes, 21)
+// opening screen will be options for no of rows/ ocrtaves/ octave sets..?
+// and number of beats per loop.  will need horizontal scrolling for large numbers 
+// and what key and scale (if not the same...) lkearn more music theory!!
+// and tempo
+// input values will be used to call setup()
+setup(notes)
 
 
 
@@ -46,9 +66,9 @@ const startBtn = document.querySelector('#start')
 let index = 0
 let synths = []
 
-
+// below numbers hard coded.  need to work form variables
 for (let i = 0; i < rows.length; i++) {
-    let synth = new Tone.Synth()
+    let synth = i < 21 ? new Tone.Synth() : new Tone.MembraneSynth()
     switch (true) {
         case i < 7:
             synth.oscillator.type = 'triangle'
@@ -59,52 +79,56 @@ for (let i = 0; i < rows.length; i++) {
         case i < 21:
             synth.oscillator.type = 'sawtooth'
             break
-        case i < 28:
-            synth.oscillator.type = ''
 
     }
-    if (i + 1 < 7) {
+
+    synths.push(synth)
+
+
+}
+
+synths[0].oscillator.type = 'triangle'
+synths[1].oscillator.type = 'sine'
+synths[2].oscillator.type = 'sawtooth'
+
+const gain = new Tone.Gain(0.6);
+gain.toDestination();
+
+synths.forEach(synth => synth.connect(gain));
+
+Tone.Transport.scheduleRepeat(repeat, '8n');
+Tone.Transport.start();
+
+
+function repeat(time) {
+    let step = index % 16;// <- needs to be numOfBeats
+    for (let i = 0; i < rows.length; i++) {
+        let synth = synths[i],
+            note = notes[i],
+            row = rows[i],
+            input = row.querySelector(`div:nth-child(${step + 1})`);
+        if (input.dataset.status !== 'off') synth.triggerAttackRelease(note, '8n', time);
+    }
+    index++;
+}
+
+console.log(Tone.Transport.state)
+startBtn.addEventListener('click', async () => {
+    if (Tone.Transport.state !== 'started') {
+        startBtn.innerText = 'STOP'
+        Tone.Transport.start();
+        await Tone.start();
 
     } else {
-        if (i + 1 < 14) {
-
-        }
-        synths.push(new Tone.Synth())
-
+        startBtn.innerText = 'START'
+        Tone.Transport.stop();
     }
+})
 
-    synths[0].oscillator.type = 'triangle'
-    synths[1].oscillator.type = 'sine'
-    synths[2].oscillator.type = 'sawtooth'
+/*
+this prob makes sense as a react app.
+I want each set of (7) rows/notes/sounds to be independent component
+allowing to select octave, notes, sounds, synth etc on the fly, not at beginning.
 
-    const gain = new Tone.Gain(0.6);
-    gain.toDestination();
-
-    synths.forEach(synth => synth.connect(gain));
-
-    Tone.Transport.scheduleRepeat(repeat, '8n');
-    Tone.Transport.start();
-
-    function repeat(time) {
-        let step = index % 16;// <- needs to be numOfBeats
-        for (let i = 0; i < rows.length; i++) {
-            let synth = synths[i],
-                note = notes[i],
-                row = rows[i],
-                input = row.querySelector(`input:nth-child(${step + 1})`);
-            if (input.checked) synth.triggerAttackRelease(note, '8n', time);
-        }
-        index++;
-    }
-    console.log(Tone.Transport.state)
-    startBtn.addEventListener('click', async () => {
-        // if (Tone.Transport.state !== 'started') {
-        await Tone.start();
-        console.log(Tone.Transport.state)
-        // } else {
-        //     Tone.Transport.stop();
-        // }
-
-        console.log("context started");
-    })
-}
+would also take other 'global' variables e.g tempo.  
+*/
